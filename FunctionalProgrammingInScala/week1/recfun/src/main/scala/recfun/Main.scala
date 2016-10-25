3c04220ceb0ff7a5db0ca4f5d9af0db9eb8d56d8
Ciro Caiazzo <ciro.caiazzo@scai-consulting.grupposcai.it>
2016-10-25 20:14:52 +0200

    wip

--- FunctionalProgrammingInScala/week1/recfun/src/main/scala/recfun/Main.scala
+++ FunctionalProgrammingInScala/week1/recfun/src/main/scala/recfun/Main.scala
@@ -26,8 +26,49 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    def balance(chars: List[Char]): Boolean = {
+      def build(list:List[Char],tmp:String): String = {
+        if(!list.isEmpty) {
+          list.head match {
+            case '(' =>  build(list.tail, tmp + '(')
+            case ')' => build(list.tail, tmp +  ')')
+            case _ => build(list.tail, tmp)
+          }
+        } else{
+          tmp
+        }
+      }
+
+      def opened(list:List[Char],count:Int): Int = {
+        list.head match {
+          case '(' =>  opened(list.tail,count +1)
+          case _  => count
+        }
+      }
+
+      def closed(list:List[Char],count:Int): Int = {
+        list.head match {
+          case ')' =>  closed(list.tail,count +1)
+          case _  => count
+        }
+      }
+
+      def check(list:List[Char]):Boolean = {
+        if(!list.isEmpty)  {
+          if(list.head != list.last) {
+            check(list.tail.dropRight(1))
+          } else {
+            false
+          }
+        } else {true}
+      }
+      val newList = build(chars,"").toList
+
+    if(newList.length % 2 == 0) check(newList)
+    else false
+    }
+
+
   /**
    * Exercise 3
    */
