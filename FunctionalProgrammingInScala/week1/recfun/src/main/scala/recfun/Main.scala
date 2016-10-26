03d4e73f9d6cf843efcca095013c53a25cd50a27
Ciro Caiazzo <ciro.caiazzo@scai-consulting.grupposcai.it>
2016-10-26 09:14:32 +0200

    wip

--- FunctionalProgrammingInScala/week1/recfun/src/main/scala/recfun/Main.scala
+++ FunctionalProgrammingInScala/week1/recfun/src/main/scala/recfun/Main.scala
@@ -27,69 +27,28 @@ object Main {
    * Exercise 2
    */
     def balance(chars: List[Char]): Boolean = {
-      def build(list:List[Char],tmp:String): String = {
-        if(!list.isEmpty) {
-          list.head match {
-            case '(' =>  build(list.tail, tmp + '(')
-            case ')' => build(list.tail, tmp +  ')')
-            case _ => build(list.tail, tmp)
-          }
-        } else{
-          tmp
-        }
-      }
-
-      def opened(list:List[Char],count:Int): Int = {
-        if (list.isEmpty) count
+      def check(list:List[Char],count:Int): Boolean = {
+        if (list.isEmpty || count < 0) count == 0
         else {
           list.head match {
-            case '(' =>  opened(list.tail,count +1)
-            case _  => count
-          }
-        }
-
-      }
-
-      def closed(list:List[Char],count:Int): Int = {
-        if (list.isEmpty) count
-        else {
-          list.head match {
-            case ')' =>  closed(list.tail,count +1)
-            case _  => count
+            case '(' => check(list.tail, count + 1)
+            case ')' => check(list.tail, count - 1)
+            case _ => check(list.tail, count)
           }
         }
       }
 
-       def check1(list:List[Char], count:Int):Boolean = {
-          if(list.isEmpty) count == 0
-          else {
-            list.head match {
-              case '(' => check1(list.tail, count + 1)
-              case ')' => check1(list.tail, count - 1)
-              case _ => check1(list.tail, count)
-            }
-          }
-       }
-
-      def check(list:List[Char]):Boolean = {
-        if (!list.isEmpty) {
-          val count = opened(list, 0)
-
-          if (list.length >= count &&  count == closed(list.drop(count), 0)) {
-            check(list.drop(count*2))
-          } else false
-
-        } else true
-      }
-      val newList = build(chars,"").toList
-
-    if(newList.length % 2 == 0) check1(newList,0)
-    else false
+        check(chars,0);
     }
 
 
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else if (money <= 0 && !coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
   }
